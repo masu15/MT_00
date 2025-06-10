03af75aeaf91df4eb2af647123fddaaacddc0b2d
ます <k024g1021@g.neec.ac.jp>
2025-06-10 10:59:03 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <Novice.h>
 #include"math.h"
-#include <Matrix4x4.h>
 #include <Vector3.h>
 const char kWindowTitle[] = "GC2D_06_マスヤマ_リョウタ_タイトル";
 const int kWindowWidte = 1280;
@@ -9,35 +8,9 @@ struct Matrix4x4
 {
 	float m[4][4];
 };
-Matrix4x4 MakeRotateXMatrix(float radian)
+Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3& rotate, const Vector3& translate)
 {
-	Matrix4x4 result;
-    
-	return result;
-}
-Matrix4x4 MakeRotateYMatrix(float radian)
-{
-	Matrix4x4 result;
 
-	return result;
-}
-Matrix4x4 MakeRotateZMatrix(float radian)
-{
-	Matrix4x4 result;
-
-	return result;
-}
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
-{
-	Matrix4x4 result = {};
-	for (int x = 0; x < 4; x++) {
-		for (int y = 0; y < 4; y++) {
-			for (int z = 0; z < 4; z++) {
-				result.m[y][x] = m1.m[x][y] * m2.m[z][x];
-			}
-		}
-	}
-	return result;
 }
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
@@ -72,11 +45,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		Vector3 rotate{ 0.4f,-1.43f,-0.8f };
-		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+		Vector3 scale{ 1.2f,0.79f,-2.1f };
+		Vector3 rotate{ 0.4f,1.43f,-0.8f };
+		Vector3 scale{ -2.7f,-4.15f,1.57f };
 		///
 		/// ↑更新処理ここまで
 		///
@@ -84,9 +55,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
-		MatrixScreenPrintf(0, kRowHeight*5, rotateYMatrix, "rotateYMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5*2, rotateZMatrix, "rotateZMatrix");
+	
 		///
 		/// ↑描画処理ここまで
 		///
