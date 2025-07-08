1be329871aa51f2c532d6781c8ed2b3db2d297c9
ます <k024g1021@g.neec.ac.jp>
2025-07-08 10:39:18 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
-#include"math.h"
-#include <Vector3.h>
+#include"cmath"
+
 const char kWindowTitle[] = "GC2D_06_マスヤマ_リョウタ_タイトル";
 const int kWindowWidte = 1280;
 const int kWindowHaight = 720;
@@ -8,18 +8,166 @@ struct Matrix4x4
 {
 	float m[4][4];
 };
+struct Vector3
+{
+	float x;
+	float y;
+	float z;
+};
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result{};
+	for (int x = 0; x < 4; x++) {
+		for (int y = 0; y < 4; y++) {
+			result.m[x][y] = m1.m[x][0] * m2.m[0][y] + m1.m[x][1] * m2.m[1][y] + m1.m[x][2] * m2.m[2][y] + m1.m[x][3] * m2.m[3][y];
+		}
+	}
+	return result;
+}
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = 1;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+	result.m[1][0] = 0;
+	result.m[1][1] = 1;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1;
+	result.m[2][3] = 0;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1;
+	return result;
+}
+Matrix4x4 MakeScaleMatrix(const Vector3& scale)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = scale.x;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+	result.m[1][0] = 0;
+	result.m[1][1] = scale.y;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = scale.z;
+	result.m[2][3] = 0;
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = 1;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+	result.m[1][0] = 0;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[1][3] = 0;
+	result.m[2][0] = 0;
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
+Matrix4x4 MakeRotateYMatrix(float radian)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = 0;
+	result.m[0][2] = -std::sin(radian);
+	result.m[0][3] = 0;
+	result.m[1][0] = 0;
+	result.m[1][1] = 1;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+	result.m[2][0] = std::sin(radian);
+	result.m[2][1] = 0;
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
+Matrix4x4 MakeRotateZMatrix(float radian)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1;
+	result.m[2][3] = 0;
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
 Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3& rotate, const Vector3& translate)
 {
-
+	Matrix4x4 result{};
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+	result.m[0][0] = scale.x * rotateXYZMatrix.m[0][0];
+	result.m[0][1] = scale.x * rotateXYZMatrix.m[0][1];
+	result.m[0][2] = scale.x * rotateXYZMatrix.m[0][2];
+	result.m[0][3] = 0;
+	result.m[1][0] = scale.y * rotateXYZMatrix.m[1][0];
+	result.m[1][1] = scale.y * rotateXYZMatrix.m[1][1];
+	result.m[1][2] = scale.y * rotateXYZMatrix.m[1][2];
+	result.m[1][3] = 0;
+	result.m[2][0] = scale.z * rotateXYZMatrix.m[2][0];
+	result.m[2][1] = scale.z * rotateXYZMatrix.m[2][1];
+	result.m[2][2] = scale.z * rotateXYZMatrix.m[2][2];
+	result.m[2][3] = 0;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1;
+	return result;
 }
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth * 1, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix,const char*label){
-	Novice::ScreenPrintf(x, 20 + y, "%s", label);
+	Novice::ScreenPrintf(x, y, "%s", label);
 	for (int row = 0; row < 4; ++row) {
 		for (int column = 0; column < 4; ++column) {
 			Novice::ScreenPrintf(
-				x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+				x + column * kColumnWidth, 20+y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
 		}
 	}
 }
@@ -56,7 +204,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		MatrixScreenPrintf(0, 0, worldMatrix, "worldMarix");
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
